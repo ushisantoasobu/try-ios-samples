eb96c398c6117bd9c6de1f77659cb1f4a6fd6749
shunsuke sato <babblemann.shunsee@gmail.com>
2022-12-21 23:34:54 +0900

    add ThinkingInSwiftUI app

---
+++ ThinkingInSwiftUI/ThinkingInSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ThinkingInSwiftUI
+//
+//  Created by Shunsuke Sato on 2022/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ThinkingInSwiftUI/ThinkingInSwiftUI/ThinkingInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkingInSwiftUIApp.swift
+//  ThinkingInSwiftUI
+//
+//  Created by Shunsuke Sato on 2022/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ThinkingInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
