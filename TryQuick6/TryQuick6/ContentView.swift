1feccdb73d71f17b60b3bd174af7052a4812ae5d
shunsuke sato <babblemann.shunsee@gmail.com>
2023-01-22 12:52:57 +0900

    add TryQuick6

---
+++ TryQuick6/TryQuick6/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TryQuick6
+//
+//  Created by Shunsuke Sato on 2023/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TryQuick6/TryQuick6/TryQuick6App.swift
@@ -0,0 +1,17 @@
+//
+//  TryQuick6App.swift
+//  TryQuick6
+//
+//  Created by Shunsuke Sato on 2023/01/22.
+//
+
+import SwiftUI
+
+@main
+struct TryQuick6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
